c530fd8b94305d132bf2126b6d938bfff1199a62
dev-dksc104638 <dev-dksc104638@depd25sh0f7gq18.dcsg.com>
2018-09-14 13:11:57 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.send('OMG OMG CloudFoundry');
+});
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});

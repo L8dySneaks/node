b2788ff0af7e1f7cade709096248d21e7fc856a8
dev-dksc104638 <dev-dksc104638@depd25sh0f7gq18.dcsg.com>
2018-09-17 12:54:38 -0400

    testing an update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
-  res.send('OMG OMG my first CloudFoundry app and successful pipeline');
+  res.send('New test on Monday');
 });
 
 

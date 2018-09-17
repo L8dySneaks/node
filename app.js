f6946404bcd892f80ab6a6330f8218357ad20bd1
dev-dksc104638 <dev-dksc104638@depd25sh0f7gq18.dcsg.com>
2018-09-17 15:34:15 -0400

    add HTML file

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var moduleTest = require('./module1.js');
 
-var fs = require('fs');
-var data = require('./data.json');
+// var fs = require('fs');
+// var data = require('./data.json');
 
 const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
@@ -11,11 +11,13 @@ app.get('/', function (req, res) {
 });
 
 
-fs.readFile('./data.json', 'utf-8', (err,data) => {
-  var data = JSON.parse(data);
-  console.log(data.name);
-});
+// fs.readFile('./data.json', 'utf-8', (err,data) => {
+//   var data = JSON.parse(data);
+//   console.log(data.name);
+// });
 
+
+app.use(express.static(__dirname));
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });

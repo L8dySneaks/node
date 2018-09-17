9fe329c6a5a5856b99e2ce6fc76a3a419384dbb8
dev-dksc104638 <dev-dksc104638@depd25sh0f7gq18.dcsg.com>
2018-09-17 14:35:31 -0400

    testing out reading data

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 var moduleTest = require('./module1.js');
+
+var fs = require('fs');
+var data = require('./data.json');
+
 const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.send(moduleTest.text1);
 });
 
+
+fs.readFile('./data.json', 'utf-8', (err,data) => {
+  var data = JSON.parse(data);
+  data.send(data.name);
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);

ffee9f3fed5281e04ce97cc09df08b719b8e10bb
dev-dksc104638 <dev-dksc104638@depd25sh0f7gq18.dcsg.com>
2018-09-18 15:10:21 -0400

    revert changes to package file

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var moduleTest = require('./module1.js');
 
-app.use(express.static(__dirname))
+// var fs = require('fs');
+// var data = require('./data.json');
+
+const port = process.env.PORT || 3000;
+// app.get('/', function (req, res) {
+//   res.send(moduleTest.text1);
+// });
+
+
+// fs.readFile('./data.json', 'utf-8', (err,data) => {
+//   var data = JSON.parse(data);
+//   console.log(data.name);
+// });
+
+
+app.use(express.static(__dirname));
 
 var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
+  {name: 'Steph', message: 'new app'},
+  {name: 'Jim', message: 'testing new app'}
 ]
 
-app.get('/messages', (req, res) =>{
-    res.send(messages)
-})
 
-var server = app.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
-})
+app.get('/mesages', (req, res) =>{
+  response.send(messages);
+});
+
+app.post('/mesages', (req, res) =>{
+  console.log(req.body);
+  response.sendStatus(200);
+});
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
